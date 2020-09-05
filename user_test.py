7f4b51cff904d312570e04ca4cb7419d2e777e26
AmimoG <gilbertmatete6@gmail.com>
2020-09-05 07:57:18 +0300

    Add Files

--- user_test.py
+++ user_test.py
@@ -3,18 +3,18 @@ from  user import User
 
 class TestUser(unittest.TestCase):
     def setUp(self):
-        self.new_user = User("Collins","Kariuki","collins@gmail.com",)
+        self.new_user = User("Amimo","Matete","gilbertmatete6@gmail.com",)
     def test1(self):
-        self.assertEqual(self.new_user.f_name,"Collins")
-        self.assertEqual(self.new_user.m_name,"Kariuki")
-        self.assertEqual(self.new_user.e_mail,"collins@gmail.com")
+        self.assertEqual(self.new_user.f_name,"Amimo")
+        self.assertEqual(self.new_user.m_name,"Matete")
+        self.assertEqual(self.new_user.e_mail,"gilbertmatete6@gmail.com")
     def test_save_user(self):
         self.new_user.save_user()
     def tearDown(self):
         User.userlist = []
     def test_delete_user(self):
         self.new_user.save_user()
-        test_data= User("ariana","gathoni","ariana@gmail.com")
+        test_data= User("elizabeth","gatwiri","elizagatwiri@gmail.com")
         test_data.save_user()
         self.assertEqual(len(User.user_list),2)
     def test_display_user(self):
